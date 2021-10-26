d002a0d0fa2ee7095989c12cee88811856d51d53
Rhyan Inacio <rhyanalves68@gmail.com>
2021-10-25 21:44:50 -0300

    create code calc

--- CalculadoraDeNotas/App.js
+++ CalculadoraDeNotas/App.js
@@ -2,23 +2,50 @@ import React, { Component } from "react"
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { nota1: 0, nota2: 0, faltas: 0, resultado: "" }
+    this.calcular = this.calcular.bind(this)
+
+  }
+  calcular() {
+    let num1 = parseInt(this.state.nota1)
+    let num2 = parseInt(this.state.nota2)
+    let media = (num1 + num2) / 2
+
+    if (media >= 6 && this.state.faltas <= 10) {
+      this.setState({ resultado: "Aprovado" })
+    } else {
+      this.setState({ resultado: "Reprovado" })
+    }
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={(styles.textInput)}
-          keyboardType="numeric" placeholder="Nota 1" />
-        <TextInput style={(styles.textInput)}
-          keyboardType="numeric" placeholder="Nota 2" />
-        <TextInput style={(styles.textInput)}
-          keyboardType="numeric" placeholder="Nº de Faltas" />
 
-        <TouchableOpacity style={styles.button}>
+        <TextInput style={(styles.textInput)}
+          onChangeText={(nota1) => { this.setState({ nota1 }) }}
+          keyboardType="numeric" placeholder="Nota 1"
+        />
+        <TextInput style={(styles.textInput)}
+          onChangeText={(nota2) => { this.setState({ nota2 }) }}
+          keyboardType="numeric" placeholder="Nota 2"
+        />
+        <TextInput style={(styles.textInput)}
+          onChangeText={(faltas) => { this.setState({ faltas }) }}
+          keyboardType="numeric" placeholder="Nº de Faltas"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={this.calcular}>
           <Text style={styles.textButon}>
             Calcular
           </Text>
         </TouchableOpacity>
         <Text style={styles.textResultado}>
-          Aluno foi Aprovado
+          {this.state.resultado}
         </Text>
       </View>
     )
